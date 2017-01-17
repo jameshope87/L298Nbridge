63bf961481c779dd8967a2b631597234efb049bb
James <james.hope87@gmail.com>
2017-01-17 17:42:53 +0000

    Fixed the broken constructor

--- L298Nbridge.cpp
+++ L298Nbridge.cpp
@@ -8,17 +8,17 @@ James' L298 Motor controller library
 
 
 DCMotor::DCMotor(uint8_t num, uint8_t inPin1, uint8_t inPin2, uint8_t enPin) {
-  motorNum = num;
+  _motorNum = num;
   
-  _inPin1s[motorNum - 1] = inPin1;
-  _inPin2s[motorNum - 1] = inPin2;
-  _enPins[motorNum - 1] = enPin;
+  _inPin1s[_motorNum - 1] = inPin1;
+  _inPin2s[_motorNum - 1] = inPin2;
+  _enPins[_motorNum - 1] = enPin;
 
 }
 
 void DCMotor::begin() {
-  pinMode(_inPin1s[motorNum - 1], OUTPUT);
-  pinMode(_inPin2s[motorNum - 1], OUTPUT);
+  pinMode(_inPin1s[_motorNum - 1], OUTPUT);
+  pinMode(_inPin2s[_motorNum - 1], OUTPUT);
   pinMode(enPin, OUTPUT);
 }
 
@@ -27,23 +27,23 @@ void DCMotor::run(uint8_t cmd) {
   
   switch(cmd) {
     case FORWARD:
-      digitalWrite(_inPin1s[motorNum - 1], HIGH);
-      digitalWrite(_inPin2s[motorNum - 1], LOW);
+      digitalWrite(_inPin1s[_motorNum - 1], HIGH);
+      digitalWrite(_inPin2s[_motorNum - 1], LOW);
       break;
     case BACKWARD:
-      digitalWrite(_inPin1s[motorNum - 1], LOW);
-      digitalWrite(_inPin2s[motorNum - 1], HIGH);
+      digitalWrite(_inPin1s[_motorNum - 1], LOW);
+      digitalWrite(_inPin2s[_motorNum - 1], HIGH);
       break;
     case BRAKE:
-      digitalWrite(_inPin1s[motorNum - 1], LOW);
-      digitalWrite(_inPin2s[motorNum - 1], LOW);
+      digitalWrite(_inPin1s[_motorNum - 1], LOW);
+      digitalWrite(_inPin2s[_motorNum - 1], LOW);
       break;
     case RELEASE:
-      digitalWrite(_enPins[motorNum - 1], LOW);
+      digitalWrite(_enPins[_motorNum - 1], LOW);
       break;
   }
 }
 
 void DCMotor::setSpeed(uint8_t speed) {
-  analogWrite(_enPins[motorNum - 1], speed);
+  analogWrite(_enPins[_motorNum - 1], speed);
 }
